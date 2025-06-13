bbb9835fc79260725ab77e5e02dd7628b44cc511
bilza2023 <bilza2023@gmail.com>
2025-06-14 00:58:59 +0500

    0.0.3

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
+
 import { DeckBuilder } from "./src/deckBuilder/deckBuilder.js";
 
 import * as defaultItems from "./src/items/defaultItems.js";
-import { GlobalThemes } from "./src/theme/globalThemes.js";
-import { GlobalBackgrounds } from "./src/theme/globalBackgrounds.js";
+import * as GobalThemes from "./src/theme/globalThemes.js";
+import * as GlobalBackgrounds  from "./src/theme/globalBackgrounds.js";
 import * as ItemBuilders from "./src/items/ItemBuilders.js";
 
 export {
   DeckBuilder,
   defaultItems,
-  GlobalThemes,        // read-only access; not injected into JSON
+  GobalThemes,
   GlobalBackgrounds,   // used in Player and DeckBuilder background fallback
   ItemBuilders         // atomic item constructors, used in slides
 };
